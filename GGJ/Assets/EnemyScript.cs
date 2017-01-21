699dd7d8d6f265fe2bdad4b5b63f0bebea726357
momentumrick <impetrified@hotmail.com>
2017-01-21 15:09:11 -0500

    3d implementation

---
+++ GGJ/Assets/EnemyScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyScript : MonoBehaviour {
+
+    NavMeshAgent agent;
+    Transform player;
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent> ();
+        player = GameObject.Find ( "Player" ).transform;
+        agent.updateRotation = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        agent.destination = player.position;
+		
+	}
+}
---
+++ GGJ/Assets/PlayerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+    
+    public float maxSpeed, noiseSpeed;
+    const float resetSpeed = .02f;
+    Vector3 movement;
+    public AudioClip step;
+    public AudioSource m_audioSource;
+
+	// Use this for initialization
+	void Start () {
+        noiseSpeed = maxSpeed - .3f;
+        m_audioSource = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ( Input.GetButton ( "Run" ) )
+            maxSpeed += .001f;
+        else
+            maxSpeed += resetSpeed;
+        movement.x = Input.GetAxis ( "Horizontal" ) * maxSpeed;
+        movement.y = Input.GetAxis ( "Vertical" ) * maxSpeed;
+    }
+
+    void FixedUpdate () {
+            
+        Vector3 speed = movement;
+        if ( speed.magnitude > noiseSpeed )
+            m_audioSource.PlayOneShot (step);
+
+        transform.position += speed;
+    }
+}
